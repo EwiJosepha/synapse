03eb90535c347d3951e6517761d39dbf940441fe
ewi josepha <ewijosepha@gmail.com>
2024-08-06 20:29:04 +0200

    fixed folder structure and fixing failed deployment

--- src/core/pages/users-profile-details/general.tsx
+++ src/core/pages/users-profile-details/general.tsx
@@ -3,6 +3,12 @@ import React from 'react'
 function General() {
   return (
     <div className='bg-red-500 '>
+      <div>
+        incoporating video in the chat app which means vid calls will be possible
+      </div>
+      <div>
+        incoporating video in the chat app which means vid calls will be possible
+      </div>
       General
     </div>
   )
