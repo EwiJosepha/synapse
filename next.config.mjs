f59ec1bbace253c17c23fe562ea51da0f791d249
solonkonora <nkwadanora@gmail.com>
2024-08-26 15:44:23 +0100

    .

--- next.config.mjs
+++ next.config.mjs
@@ -1,26 +1,4 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {};
-
-// export default nextConfig;
-
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    images: {
-      domains: ['www.google.com', 'iconscout.com', 'freepik.com', 'vecteezy.com', 'icons8.com', 'storyset.com', 'svgrepo.com'],
-    },
-    // Add CORS configuration
-    async rewrites() {
-      return [
-        {
-          source: '/api/:path*',
-          destination: 'http://localhost:3000/api/:path*', // Replace with your actual API server URL
-        },
-      ];
-    },
-    // Set HTTP port
-    port: "", // Adjust to your desired port
-    // Set public path name
-    basePath: '/my-app', // Adjust to your desired path name
-  };
-  
-  export default nextConfig;
+const nextConfig = {};
+
+export default nextConfig;
