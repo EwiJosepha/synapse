7b96736b28765ff39379ee551224f5773b6bada2
ewi josepha <ewijosepha@gmail.com>
2024-08-30 15:00:25 +0200

    establishing real time msg transefer between sender  and user

--- src/core/components/messageList.tsx
+++ src/core/components/messageList.tsx
@@ -1,48 +1,33 @@
-// MessageList.tsx
-import { useAppContext } from '@/providers/context/app-context';
 import React, { useEffect, useState } from 'react';
-// import ChatMessage from './chatMessage';
+import { MessageSchema } from '@/lib/interfaces/message';
+import { fetchCurrentUser } from '../../../utils/currentUser';
 
-interface Message {
-  // id: number;
-  content: string; // Use 'content' instead of 'text'
-  emoji: string | null;
-  receiverId :string | null,
-  senderId: string | null,
-  conversationId : string,
-  reactions: string[],
-  attachments: string[]
-}
-
-// interface MessageListProps {
-//   messages: Message[];
-// }
-
-interface ContentMessages {
-  content: string[]
-}
-
-const MessageList: React.FC<{ messages: Message[] }> = ({ messages }) => {
-  const { userMessages, setUserMessages } = useAppContext()
-  const [contentMessages, setContentMessages] = useState<ContentMessages>()
+const MessageList: React.FC<{ messages: MessageSchema[] }> = ({ messages }) => {
+  const [currentId, setCurrentId] = useState("");
 
   useEffect(() => {
-    if (Array.isArray(userMessages)) {
-      const userMessageContents = userMessages.map(message => message.content);
-      const messageContents = userMessages.map(message => message.content);
-      setContentMessages({ content: messageContents });
-      const initialContents = messages.map(message => message.content);
-      // setContentMessages({ content: [...initialContents, ...userMessageContents] });
-    }
-  }, [userMessages, messages]);
+    const getCurrentUser = async () => {
+      try {
+        const currentUser = await fetchCurrentUser();
+        setCurrentId(currentUser.data.id); 
+      } catch (error) {
+        console.error("Error fetching current user:", error);
+      }
+    };
 
-  // const combinedMessages = contentMessages?.content.concat(message ? [message] : []);
+    getCurrentUser();
+  }, []);
 
   return (
     <div className="flex-2 overflow-y-auto flex flex-col space-y-4 mb-20 sm:mb-24 md:mb-28 max-w-[400px]">
-      {contentMessages?.content.map((msg, index) => (
-        <div key={index} className="px-4 py-2 rounded-md sm:px-6 md:px-8 bg-gray-200">
-          {msg}
+        {messages.map((msg, index) => (
+        <div 
+          key={index} 
+          className={`px-4 py-2 rounded-md sm:px-6 md:px-8 ${
+            msg.senderId === currentId ? 'bg-blue-200 ml-auto' : 'bg-gray-200 mr-auto'
+          }`}
+        >
+          <p>{msg.content}</p>
         </div>
       ))}
     </div>
